3fe3a81ddcb77adfbea153aaf2de3ced4282510c
cwyyyds <1013953614@qq.com>
2022-06-11 09:51:46 +0800

    index完成

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,7 +11,16 @@ function getUserInfo() {
             //调用渲染函数
             renderAvatar(res.data)
             console.log(res);
-        }
+        },
+        // complete: (res) => {
+        //     // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
+        //     if(res.responseJSON.status ===1 && res.responseJSON.message === "身份认证失败！") {
+        //         //  强制清空 token
+        //         localStorage.removeItem("token");
+        //         // 强制跳转到登录页面
+        //         location.href = "/login.html"
+        //     }
+        // },
     })
 }
 
